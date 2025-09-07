6319e548f66ed08c35a9ded4bd60afc3f393d3f8
paventhan <paventhanbtech1990@gmail.com>
2025-09-07 13:17:05 +0530

    changes added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ListPage from './components/ListPage';
 import DetailsPage from './components/DetailsPage';
 import './App.css';
-
+import { HashRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     // <Router>
