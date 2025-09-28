f1d913857061428aa4595227e5846a575b609a40
paventhan <paventhanbtech1990@gmail.com>
2025-09-28 15:38:14 +0530

    added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <div className="advertisement-contact">
-        For Advertisement contact: 1234567890
+        For Advertisement contact: 
       </div>
       <HashRouter>
         <Routes>
